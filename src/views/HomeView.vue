c5287bda5211449220f5e50d83b573d4ae1d3a6f
kerinhanabi <kerinhanabi@hotmail.com>
2024-08-28 20:50:15 +0800

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <h1>Homepage</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi sed ex similique magnam recusandae facere distinctio ad, praesentium excepturi, modi nesciunt neque molestiae qui laudantium corporis in officiis? Ut id alias assumenda vero, ullam unde accusamus perferendis nihil voluptas aliquam quaerat? Eius asperiores, repellendus, dolorem exercitationem itaque tenetur natus non, molestias atque numquam ullam? Cupiditate eum asperiores nostrum aspernatur ullam quae totam facilis omnis sit minus repellat nam deserunt, quibusdam, est similique eveniet porro odit aliquam eaque. Repellendus nulla animi, adipisci esse quam dolor earum possimus, aspernatur, dicta dignissimos dolore odio fugiat ea? Consectetur distinctio, quibusdam sapiente maxime quidem rerum?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi sed ex similique magnam recusandae facere distinctio ad, praesentium excepturi, modi nesciunt neque molestiae qui laudantium corporis in officiis? Ut id alias assumenda vero, ullam unde accusamus perferendis nihil voluptas aliquam quaerat? Eius asperiores, repellendus, dolorem exercitationem itaque tenetur natus non, molestias atque numquam ullam? Cupiditate eum asperiores nostrum aspernatur ullam quae totam facilis omnis sit minus repellat nam deserunt, quibusdam, est similique eveniet porro odit aliquam eaque. Repellendus nulla animi, adipisci esse quam dolor earum possimus, aspernatur, dicta dignissimos dolore odio fugiat ea? Consectetur distinctio, quibusdam sapiente maxime quidem rerum?</p>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: 'Home',
+  components: {
+    
+  }
+}
+</script>
